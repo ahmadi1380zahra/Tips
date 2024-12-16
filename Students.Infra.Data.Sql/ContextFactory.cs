7c1e82b8ad0f4243fabf79f8fded7d62493cd92e
unknown <zahraahmaadi1380@gmail.com>
2024-12-16 07:18:35 -0800

    fix: migartion

--- Students.Infra.Data.Sql/ContextFactory.cs
+++ Students.Infra.Data.Sql/ContextFactory.cs
@@ -8,7 +8,7 @@ namespace Students.Infra.Data.Sql
         public static Context GetSqlContext()
         {
             var builder = new DbContextOptionsBuilder<Context>();
-            builder.UseSqlServer("Server=.;Database=StudentBromand;User ID = sa; Password = 123;TrustServerCertificate = True",c=>c.UseQuerySplittingBehavior( QuerySplittingBehavior.SplitQuery))
+            builder.UseSqlServer("Server=.;Database=StudentBromand;Integrated Security=true;TrustServerCertificate=true;",c=>c.UseQuerySplittingBehavior( QuerySplittingBehavior.SplitQuery))
                 .LogTo(Console.WriteLine,Microsoft.Extensions.Logging.LogLevel.Information);
             return new Context(builder.Options);
         }
